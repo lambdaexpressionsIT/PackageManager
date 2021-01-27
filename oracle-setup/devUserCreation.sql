f54e4802400532c52d82de73664c587977be9208
Stefano Mancini <stefano.mancini.1983@gmail.com>
2021-01-27 16:26:16 +0100

    Version 1.0.0

--- oracle-setup/devUserCreation.sql
+++
@@ -1,9 +0,0 @@
-/*
- * from SYS user, create an user that can create a table
- */
-
-CREATE USER package_manager IDENTIFIED BY "pAcMaN_1337_@$!";
-GRANT CREATE SESSION to package_manager;
-grant create table to package_manager;
-grant resource to package_manager;
-alter user package_manager quota unlimited on users;
